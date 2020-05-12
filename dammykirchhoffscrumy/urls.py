a528c38c734ce51933a76a57c12dab63ccab624c
dammyjay <dammykirchhoff@gmail.com>
2020-05-12 12:26:58 +0100

    new

---
+++ dammykirchhoffscrumy/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path ('', views.get_grading_parameters, name = 'get_grading_parameters')
+]
---
+++ dammykirchhoffscrumy/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+def get_grading_parameters(request):
+    return HttpResponse("Welcome to Django dammy")
